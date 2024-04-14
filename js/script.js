dc8c14aa8983eef5e8b1d740fdf6270fa57ee3af
@MarinaGAguirre <maguirre.neticel@gmail.com>
2024-04-14 17:13:23 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    centerSlide: "true",
+    grabCursor: "true",
+    fade: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dinamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4
+      },
+    },
+  });
